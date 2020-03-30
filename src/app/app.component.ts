f0abc4e6b4f5f62bc86c2f59bf012df86cb0d347
Ayyaz <you@example.com>
2020-03-30 17:31:44 +0500

    In this section of code created toDotask list static data show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'toDotask';
+  title = 'todo';
+  inputText="";
+
+data=[
+  {"task":"taskOne","created":"10 days ago"},
+  {"task":"taskTwo","created":"15 days ago"},
+  {"task":"taskThree","created":"20 days ago"}
+]
+
+ create(){
+
+  this.data.unshift({"task":this.inputText,"created":"few second ago"});
+  this.inputText="";
 }
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatToolbarModule} from '@angular/material/toolbar'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
